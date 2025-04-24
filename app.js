500c776a637e7050d050913181b2579d72a3b328
Vincent Loh <vincentmloh87@gmail.com>
2025-04-24 11:56:58 -0400

    functional prototype

--- app.js
+++ app.js
@@ -1,24 +1,79 @@
-const ROWS = 8, COLS = 8, SZ = 40;
+const ROWS = 8, COLS = 8, SZ = 40, E = 5, SPEEDS = [1, 5, 10, 100];
 
 let graph = new Array(ROWS*COLS).fill(1),
-	weights = new Array(ROWS*COLS);
+	weight = new Array(ROWS*COLS);
 
 for(let i=0; i<ROWS*COLS; ++i)
-	weights[i] = new Array(ROWS*COLS).fill(1);
+	weight[i] = new Array(ROWS*COLS).fill(0);
 
 function setup(){
 	createCanvas(windowWidth, windowHeight);
+	textAlign(CENTER, CENTER);
+	textSize(16);
+	noStroke();
 }
+
+let col = 0, run = false, speed = SPEEDS[0], memories = 0;
 
 function draw(){
 	background(100);
 
-	// draw graph
-	
-	noStroke();
+	if(run) for(let i=0; i<speed; ++i){
+		const C = Math.floor(Math.random()*ROWS*COLS);
+		let V = 0; for(let j=0; j<ROWS*COLS; ++j) V += weight[C][j] * graph[j];
+		graph[C] = V < 0 ? -1 : 1;
+	}
+
 	for(let i=0; i<ROWS; ++i)
 		for(let j=0; j<COLS; ++j){
 			fill((graph[i*COLS+j]+1)*255/2);
-			rect(width/2-SZ*COLS/2 + SZ*j, height/2-SZ*ROWS/2 + SZ*i, SZ, SZ);
+			rect(width/2-SZ*COLS/2 + SZ*j + E/2, height/2-SZ*ROWS/2 + SZ*i + E/2, SZ-E, SZ-E);
+			if(mouseIsPressed && Math.abs(width/2-SZ*COLS/2 + SZ*j + SZ/2 - mouseX) < SZ/2 && Math.abs(height/2-SZ*ROWS/2 + SZ*i + SZ/2 - mouseY) < SZ/2){
+				if(col == 0) col = -graph[i*COLS+j];
+				graph[i*COLS+j] = col;
+			}
 		}
+
+	fill(225);
+	
+	button(run ? "PAUSE" : "RUN", width/2, height/2-SZ*ROWS/2-80);
+	button("SPEED: " + speed.toString(), width/2, height/2-SZ*ROWS/2-40);
+
+	button("CLEAR", width/2, height/2+SZ*ROWS/2+40);
+	button("RESET MEMORIES", width/2, height/2+SZ*ROWS/2+80);
+	button("ADD MEMORY (" + memories.toString() + " stored)", width/2, height/2+SZ*ROWS/2+120);
+}
+
+function button(T, x, y){
+	textSize(mouseIn(x, y, 150, 20) ? 17 : 16);
+	text(T, x, y);
+}
+
+function mouseIn(x, y, w, h){
+	return abs(x-mouseX) < w && abs(y-mouseY) < h;
+}
+
+function mouseReleased(){
+	col = 0;
+
+	if(mouseIn(width/2, height/2-SZ*ROWS/2-40, 150, 20)) speed = SPEEDS[(SPEEDS.indexOf(speed)+1)%SPEEDS.length];
+
+	if(mouseIn(width/2, height/2-SZ*ROWS/2-80, 150, 20)) run = !run;
+
+	if(mouseIn(width/2, height/2+SZ*ROWS/2+40, 150, 20))
+		for(let i=0; i<ROWS*COLS; ++i) graph[i] = 1;
+
+	if(mouseIn(width/2, height/2+SZ*ROWS/2+80, 150, 20)){
+		for(let i=0; i<ROWS*COLS; ++i)
+			for(let j=0; j<ROWS*COLS; ++j)
+				weight[i][j] = 0;
+		memories = 0;
+	}
+
+	if(mouseIn(width/2, height/2+SZ*ROWS/2+120, 150, 20)){
+		for(let i=0; i<ROWS*COLS; ++i)
+			for(let j=0; j<ROWS*COLS; ++j)
+				if(i != j) weight[i][j] += graph[i]*graph[j];
+		++memories;
+	}
 }
